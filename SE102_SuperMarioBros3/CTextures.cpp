76731b49e6d652dd1b236d9a4c3d38f1c710019c
20522152 <75103428+AFK-devabc@users.noreply.github.com>
2022-10-22 10:10:43 +0700

    Feat : Add Textures for texture manager

---
+++ SE102_SuperMarioBros3/CTextures.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+
+#include "debug.h"
+#include "Game.h"
+#include "CTextures.h"
+
+CTextures* CTextures::__instance = NULL;
+
+CTextures::CTextures()
+{
+
+}
+
+CTextures* CTextures::GetInstance()
+{
+	if (__instance == NULL) __instance = new CTextures();
+	return __instance;
+}
+
+
+
+void CTextures::Add(int id, LPCWSTR filePath)
+{
+	textures[id] = CGame::GetInstance()->LoadTexture(filePath);
+}
+
+LPTEXTURE CTextures::Get(unsigned int i)
+{
+	return textures[i];
+}
+
+
+
